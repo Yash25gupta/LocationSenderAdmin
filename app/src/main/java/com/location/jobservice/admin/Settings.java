8b5afba553257fc74088e9f66fb055c28b393ce9
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-05-26 14:13:09 +0530

    switch Added

--- app/src/main/java/com/location/jobservice/admin/Settings.java
+++ app/src/main/java/com/location/jobservice/admin/Settings.java
@@ -1,14 +1,31 @@
 package com.location.jobservice.admin;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Settings extends AppCompatActivity {
+    private static final String collection = "Locations";
+    private static final String sField = "sendData";
     private String target = "";
+    private SwitchCompat pauseSwitch;
+    private boolean isSendingData = false;
+    private FirebaseFirestore fStore;
+    DocumentReference docReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,38 @@ public class Settings extends AppCompatActivity {
             target = getIntent().getExtras().getString("name");
         }
 
-        Toast.makeText(this, target, Toast.LENGTH_SHORT).show();
+        pauseSwitch = findViewById(R.id.set_pauseSwitch);
+        fStore = FirebaseFirestore.getInstance();
+        docReference = fStore.collection(collection).document(target);
+
+        // Get Last sendData Status
+        docReference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document != null && document.exists()) {
+                                pauseSwitch.setChecked(document.getBoolean(sField));
+                            }
+                        }
+                    }
+                });
+
+        pauseSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isSendingData = pauseSwitch.isChecked();
+                changeSendData();
+            }
+        });
+
     }
+
+    private void changeSendData() {
+        Map<String, Boolean> send = new HashMap<>();
+        send.put(sField, isSendingData);
+        docReference.set(send, SetOptions.merge());
+    }
+
 }
